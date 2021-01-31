56bb86e59fe5aac3598c3c28e594dbfb6b64fd6f
Adam Wysocki <adwyspl@gmail.com>
2021-01-31 14:05:39 +0100

    before pid check in new_serv()

---
+++ main.c
@@ -0,0 +1,44 @@
+#include "server.h"
+
+
+int main(int argc,char ** argv){
+    del();
+    initscr();
+    noecho();
+    cbreak();
+//    refresh();
+    getmaxyx(stdscr,y,x);
+    board=newwin(20,30,0,0);
+    consola = newwin(y+10,30,11,0);
+    pthread_t Server,Client;
+
+    wprintw(consola,"hello pid:%d\n",getpid());
+    wrefresh(consola);
+    if(new_serv()){
+        wprintw(consola,"Starting server\n");
+        pthread_create(&Server,NULL,&server_thr,NULL);
+    }
+    sleep(5);
+    int canconn = -1;
+    for(int i=0;i<3;i++){
+        if(canconn == -1)
+            canconn = shm_open(SHM_SERV,O_RDWR,0666);
+        if(canconn != -1)break;
+        sleep(1);
+    }
+    if(canconn == -1){
+        wprintw(consola,"\n not connected shuting down");
+        wrefresh(consola);
+        sleep(1);
+        return 1;
+    }
+    close(canconn);
+    wprintw(consola,"\nconnected\n");
+    wrefresh(consola);
+
+    pthread_create(&Client,NULL,&client_thr,NULL);
+
+    getchar();
+    del();
+    return 0;
+}
--- server.c
+++ server.c
@@ -1,33 +1,122 @@
-#include "include.h"
+//#include "include.h"
+#include "server.h"
 
 
-int main(int argc, char** argv){
-    sem_t* sem = sem_open("/sem",O_CREAT,0600,0);
+int new_serv(){
+    int ret_val=1;
+    int serv_exist = shm_open(SHM_SERV,O_RDONLY,0666);
+    if(serv_exist != -1){
+        struct data_t* asd = (struct data_t*)mmap(NULL, sizeof(struct data_t), PROT_READ | PROT_WRITE, MAP_SHARED, serv_exist, 0);
+//        wprintw(consola,"pid=",asd->pid);
+//        wrefresh(consola);
+        if(asd == (void*)-1 ) {
+            ret_val = 0;
+        }
+        else del();
+        munmap(asd,sizeof(struct data_t));
+        }
+        close(serv_exist);
+
+    return ret_val;
+}
+void del(){
+    shm_unlink(SHM_SERV);
+    sem_unlink(COMMON_SEMAPHORE_NAME);
+}
+void *server_thr(){
+
+    sem_t* sem = sem_open(COMMON_SEMAPHORE_NAME, O_CREAT, 0600, 0);
     err(sem == SEM_FAILED, "sem_open");
-    int fd = shm_open("/msg_data", O_CREAT | O_RDWR, 0600);
+
+    int fd = shm_open(SHM_SERV, O_CREAT | O_RDWR, 0600);
     err(fd == -1, "shm_open");
 
+    ftruncate(fd, sizeof(struct data_t));
+    struct data_t* pdata = (struct data_t*)mmap(NULL, sizeof(struct data_t), PROT_READ | PROT_WRITE, MAP_SHARED, fd, 0);
+    err(pdata == NULL, "mmap");
+    pdata->pid = getpid();
+    sem_init(&pdata->cs, 1, 1); // shared, signaled
+//    printf("Gotowe, czekam na klienta; pdata=%p...,pdata->pid\n", pdata);
+    wprintw(consola,"Gotowe, czekam na klienta; pdata=%p...,pdata->pid\n", pdata);
+    wrefresh(consola);
+    while(1){
+//        if(getchar() == 'q')break;
+    }
 
-    ftruncate(fd, sizeof(struct data2_t));
-    struct data2_t* pdata = (struct data_t*)mmap(NULL, sizeof(struct data2_t), PROT_READ | PROT_WRITE, MAP_SHARED, fd, 0);
+    return NULL;
+}
+void *client_thr(){
+    wprintw(consola,"Client thread started");
+    wrefresh(consola);
+    sem_t* sem = sem_open(COMMON_SEMAPHORE_NAME, 0);
+    err(sem == SEM_FAILED, "sem_open");
+
+    int fd = shm_open(SHM_SERV, O_RDWR, 0600);
+    err(fd == -1, "shm_open");
+
+    struct data_t* pdata = (struct data_t*)mmap(NULL, sizeof(struct data_t), PROT_READ | PROT_WRITE, MAP_SHARED, fd, 0);
     err(pdata == NULL, "mmap");
 
-    sem_init(&pdata->cs, 1, 1);
-    printf("Gotowe, czekam na klienta; pdata=%p...\n", pdata);
-    int terminate = 0, counter = 0;
-    while (!terminate) {
-        pdata
-        sem_wait(sem);
-        sem_wait(&pdata->cs);
-        printf("[%03d:%03d]: %s\n", counter++, pdata->id, pdata->payload);
-        terminate = strcasecmp(pdata->payload, "quit") == 0;
-        sem_post(&pdata->cs);
+    srand(time(NULL));
+    int my_id = rand() % 1000;
+    printf("Identyfikator sesji: %d; pdata=%p......\n", my_id, pdata);
+    while(1) {
+//        move(pdata,my_id,sem);
+//        if(pdata->payload == 'q')break;
+//        sem_post(sem);
+//    printf("odpowiedz servera: %s\n",pdata->map);
     }
-    munmap(pdata, sizeof(struct data2_t));
-    close(fd);
-    shm_unlink("/msg_data");
     sem_close(sem);
-
-
+    munmap(pdata, sizeof(struct data_t));
+    close(fd);
     return 0;
 }
+
+//int printl(WINDOW *win,const char* fmt, ...)
+//{
+//    va_list ap;
+//    va_start(ap, fmt);
+//
+//    char dest[300];
+//    vsnprintf(dest, 300, fmt, ap);
+//    va_end(ap);
+//
+//    sem_wait(&nc);
+//    wprintw(win,"%s\n",dest);
+//    wrefresh(win);
+//    sem_post(&nc);
+//    return !ERR;
+//}
+
+//int main(int argc, char **argv)
+//{
+//    sem_t* sem = sem_open("/msg_signal", O_CREAT, 0600, 0);
+//    err(sem == SEM_FAILED, "sem_open");
+//
+//    int fd = shm_open("/msg_data", O_CREAT | O_RDWR, 0600);
+//    err(fd == -1, "shm_open");
+//
+//    ftruncate(fd, sizeof(struct data_t));
+//    struct data_t* pdata = (struct data_t*)mmap(NULL, sizeof(struct data_t), PROT_READ | PROT_WRITE, MAP_SHARED, fd, 0);
+//    err(pdata == NULL, "mmap");
+//
+//    sem_init(&pdata->cs, 1, 1); // shared, signaled
+//    printf("Gotowe, czekam na klienta; pdata=%p...\n", pdata);
+//
+//    int turn = 0;
+//    while (turn < 10) {
+//        show_server_map("./map.txt");
+//        take(pdata,sem,turn);
+//        if(pdata->payload == 'q')break;
+////        if(pdata->payload = 'a')strcpy(pdata->map, "UP");
+////        sem_post(&pdata->cs);
+////        sem_post(sem);
+//        sem_wait(sem);
+//        turn++;
+//    }
+//    munmap(pdata, sizeof(struct data_t));
+//    close(fd);
+//    shm_unlink("/msg_data");
+//    sem_close(sem);
+//    return 0;
+//}
